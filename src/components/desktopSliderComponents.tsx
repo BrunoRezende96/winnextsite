ee8f970abb0764a97dd6a7a2fb2015168347de8e
Bruno Rezende <brunorezende201611@gmail.com>
2025-12-20 12:12:28 -0300

    Ajustes no responsivo

--- src/components/desktopSliderComponents.tsx
+++ src/components/desktopSliderComponents.tsx
@@ -10,79 +10,23 @@ export default function DesktopSliderComponents() {
 
         {
             title:
-                <div id="title" 
-                    className=" opacity-90 container p-DPad relative flex flex-col h-[800px]" >
+                <div id="title"
+                    className=" container p-DPad relative flex flex-col h-[800px]" >
 
-                    <h1 className="
-                    
-                    *Paddings*
-                    px-8
-                    text-6xl
+                    <div className="pl-8 w-full h-auto flex justify-start">
+                        <h1 className=" w-[50%] text-white font-semibold text-start text-5xl
+                        
+                        md:w-[40%]
+                        md:top-8
+                        md:relative
+                        
+                        ">
+                            Go to your next win
+                        </h1>
+                    </div>
 
-
-                    *Positioning*
-                    flex
-                    flex-col
-                    justify-end
-
-
-                    *Breakpoints*
-                    md:text-[4rem] 
-                    leading-[0.9] 
-                    w-[80%]
-                    
-
-                    *sm BreakPoints*
-                    sm:h-[18%]
-                    sm:justify-end
-
-                    *md BreakPoints*
-                    md:w-[60%]
-                    md:justify-end
-                    md:text-6xl
-
-                    *lg BreakPoints*
-                    lg:mt-16
-                    lg:h-[30%] 
-                    lg:justify-end
-                    lg:text-8xl
-
-                    mid:w-[300px]
-                    mid:h-[10%]
-                    mid:mb-2
-                    mid:text-[2.6rem]
-                    mid:justify-center
-
-                    
-                    
-                    *Height Adjustments*
-                    h-[25%]
-
-                    
-                    *Font Formatation*
-                    font-medium 
-                    text-white 
-                    left-0
-
-                    ">
-                        Go to your next win
-                    </h1>
-
-                    <div className=" 
-
-                    flex 
-                    px-8
-                    
-
-                    *lg BreakPoints*
-                    lg:mt-2
-                    md:mt-2
-                    sm:mt-2
-
-
-                    " >
-                        <WinnextButton
-                            buttonType="small" />
+                    <div className=" p-DPad" >
+                        <WinnextButton buttonType="XS" />
                     </div>
 
                 </div>
@@ -91,22 +35,53 @@ export default function DesktopSliderComponents() {
 
         {
             title:
-                <div className=" container p-DPad relative flex flex-col h-screen" >
+                <div className=" container p-DPad relative flex flex-col h-screen gap-4" >
 
-                    <h1 className=" p-DPad absolute text-[9rem] leading-[0.9] font-medium text-white top-20 text-right right-0 w-[70%] ">
-                        Discover our solutions
-                    </h1>
+
+                    <div className="w-full flex justify-end " >
+                        <h1 className=" w-[50%] text-right text-5xl font-semibold text-white
+                        
+                        md:top-9
+                        md:relative
+                        
+                        
+                        ">
+                            Discover our solutions
+                        </h1>
+                    </div>
+                    <div className="w-full flex justify-end
+                        md:top-5
+                        md:relative                   
+                    
+                    ">
+                        <WinnextButton buttonType="XS" />
+                    </div>
+
                 </div>
         },
 
 
         {
             title:
-                <div className=" container p-DPad relative items-center flex flex-col h-screen" >
+                <div className=" container p-DPad relative items-center gap-4 flex flex-col h-screen" >
 
-                    <h1 className=" p-DPad absolute text-[7.5rem] leading-[0.9] font-medium text-white top-0 text-center  w-[80%]">
-                        Get your success started
-                    </h1>
+                    <div className="w-full relative flex justify-center top-[-30px]
+                    
+                    md:top-[-20px]
+                    
+                    " >
+                        <h1 className="  w-[60%] text-5xl text-center font-semibold text-white">
+                            Get your success started
+                        </h1>
+                    </div>
+
+                    <div className="w-full flex relative  top-[-40px] justify-center
+                    
+                    md:top-[-20px]
+                    
+                    ">
+                        <WinnextButton buttonType="XS" />
+                    </div>
                 </div>
         },
 
